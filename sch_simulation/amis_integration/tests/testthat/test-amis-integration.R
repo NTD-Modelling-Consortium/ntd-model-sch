00cd7e786393a156d41b64c93da1ef4fd897dd51
Thomas <thomas.kiley@ndm.ox.ac.uk>
2024-06-17 12:37:46 +0100

    Remove duplicated parameters from tests
    Since it doesn't change per test and isn't relevant for understanding
    the results, can reduce clutter in the tests making it easier to see
    what they are testing by moving this to the top of the file.

--- sch_simulation/amis_integration/tests/testthat/test-amis-integration.R
+++ sch_simulation/amis_integration/tests/testthat/test-amis-integration.R
@@ -2,10 +2,8 @@ setwd("../..")
 
 source("amis_integration.R")
 
-test_that("Running the model should give us some results", {
-    sch_simulation <- get_amis_integration_package()
-
-    fixed_parameters <- sch_simulation$FixedParameters(
+sch_simulation <- get_amis_integration_package()
+example_parameters <- sch_simulation$FixedParameters(
         # the higher the value of N, the more consistent the results will be
         # though the longer the simulation will take
         number_hosts = 10L,
@@ -27,39 +25,16 @@ test_that("Running the model should give us some results", {
         min_multiplier = 5L
     )
 
+test_that("Running the model should give us some results", {
     # Example prevalence map, with two locations, both with prevalence of 0.5
     prevalence_map <- matrix(c(0.5, 0.5), ncol = 1)
 
-    tranmission_model <- build_transmission_model(prevalence_map, fixed_parameters, year_indices = c(23))
+    tranmission_model <- build_transmission_model(prevalence_map, example_parameters, year_indices = c(23))
     result <- tranmission_model(c(1, 2), matrix(c(3, 3, 0.04, 0.04), ncol = 2), 1)
     expect_equal(result, matrix(c(0.1, 0.1), ncol = 1), tolerance = 0.5)
 })
 
 test_that("Running the simulation on multiple time points gives multiple points back", {
-    sch_simulation <- get_amis_integration_package()
-
-    fixed_parameters <- sch_simulation$FixedParameters(
-        # the higher the value of N, the more consistent the results will be
-        # though the longer the simulation will take
-        number_hosts = 10L,
-        # no intervention
-        coverage_file_name = "mansoni_coverage_scenario_0.xlsx",
-        demography_name = "UgandaRural",
-        # cset the survey type to Kato Katz with duplicate slide
-        survey_type = "KK2",
-        parameter_file_name = "mansoni_params.txt",
-        coverage_text_file_storage_name = "Man_MDA_vacc.txt",
-        # the following number dictates the number of events (e.g. worm deaths)
-        # we allow to happen before updating other parts of the model
-        # the higher this number the faster the simulation
-        # (though there is a check so that there can't be too many events at once)
-        # the higher the number the greater the potential for
-        # errors in the model accruing.
-        # 5 is a reasonable level of compromise for speed and errors, but using
-        # a lower value such as 3 is also quite good
-        min_multiplier = 5L
-    )
-
     # Example prevalence map, with two locations, fitting to two time times
     # Both locations start at 0.031, and the second time point is 0.021
     prevalence_map <- vector("list", 2)
@@ -68,69 +43,21 @@ test_that("Running the simulation on multiple time points gives multiple points 
 
     year_indices <- c(0L, 23L)
 
-    tranmission_model <- build_transmission_model(prevalence_map, fixed_parameters, year_indices)
+    tranmission_model <- build_transmission_model(prevalence_map, example_parameters, year_indices)
     result <- tranmission_model(c(1, 2), matrix(c(3, 3, 0.04, 0.04), ncol = 2), 1)
     expect_equal(result, matrix(c(0.1, 0.1, 0.1, 0.1), ncol = 2), tolerance = 0.5)
 })
 
 test_that("Running the simulation with different number of years specified compared to the prevalance map raises an error", {
-    sch_simulation <- get_amis_integration_package()
-
-    fixed_parameters <- sch_simulation$FixedParameters(
-        # the higher the value of N, the more consistent the results will be
-        # though the longer the simulation will take
-        number_hosts = 10L,
-        # no intervention
-        coverage_file_name = "mansoni_coverage_scenario_0.xlsx",
-        demography_name = "UgandaRural",
-        # cset the survey type to Kato Katz with duplicate slide
-        survey_type = "KK2",
-        parameter_file_name = "mansoni_params.txt",
-        coverage_text_file_storage_name = "Man_MDA_vacc.txt",
-        # the following number dictates the number of events (e.g. worm deaths)
-        # we allow to happen before updating other parts of the model
-        # the higher this number the faster the simulation
-        # (though there is a check so that there can't be too many events at once)
-        # the higher the number the greater the potential for
-        # errors in the model accruing.
-        # 5 is a reasonable level of compromise for speed and errors, but using
-        # a lower value such as 3 is also quite good
-        min_multiplier = 5L
-    )
-
     # Example prevalence map, with two locations, both with prevalence of 0.5
     prevalence_map <- matrix(c(0.5, 0.5), ncol = 1)
 
     year_indices <- c(0L, 23L)
 
-    expect_error(build_transmission_model(prevalence_map, fixed_parameters, year_indices), "Single time point prevalance map provided so should only request one year but 2 provided")
+    expect_error(build_transmission_model(prevalence_map, example_parameters, year_indices), "Single time point prevalance map provided so should only request one year but 2 provided")
 })
 
 test_that("Running the simulation with different number of years specified compared to the prevalance map raises an error", {
-    sch_simulation <- get_amis_integration_package()
-
-    fixed_parameters <- sch_simulation$FixedParameters(
-        # the higher the value of N, the more consistent the results will be
-        # though the longer the simulation will take
-        number_hosts = 10L,
-        # no intervention
-        coverage_file_name = "mansoni_coverage_scenario_0.xlsx",
-        demography_name = "UgandaRural",
-        # cset the survey type to Kato Katz with duplicate slide
-        survey_type = "KK2",
-        parameter_file_name = "mansoni_params.txt",
-        coverage_text_file_storage_name = "Man_MDA_vacc.txt",
-        # the following number dictates the number of events (e.g. worm deaths)
-        # we allow to happen before updating other parts of the model
-        # the higher this number the faster the simulation
-        # (though there is a check so that there can't be too many events at once)
-        # the higher the number the greater the potential for
-        # errors in the model accruing.
-        # 5 is a reasonable level of compromise for speed and errors, but using
-        # a lower value such as 3 is also quite good
-        min_multiplier = 5L
-    )
-
     # Example prevalence map, with two locations, fitting to two time times
     # Both locations start at 0.031, and the second time point is 0.021
     prevalence_map <- vector("list", 2)
@@ -139,35 +66,11 @@ test_that("Running the simulation with different number of years specified compa
 
     year_indices <- c(23L)
 
-    expect_error(build_transmission_model(prevalence_map, fixed_parameters, year_indices), "Length of prevalance map \\(2\\) must match the number of years provided in year_indices \\(1\\)")
+    expect_error(build_transmission_model(prevalence_map, example_parameters, year_indices), "Length of prevalance map \\(2\\) must match the number of years provided in year_indices \\(1\\)")
 })
 
 test_that("Running the AMIS integration on multiple time points should complete with the error about weight on particles", {
-    sch_simulation <- get_amis_integration_package()
-
-    fixed_parameters <- sch_simulation$FixedParameters(
-        # the higher the value of N, the more consistent the results will be
-        # though the longer the simulation will take
-        number_hosts = 10L,
-        # no intervention
-        coverage_file_name = "mansoni_coverage_scenario_0.xlsx",
-        demography_name = "UgandaRural",
-        # cset the survey type to Kato Katz with duplicate slide
-        survey_type = "KK2",
-        parameter_file_name = "mansoni_params.txt",
-        coverage_text_file_storage_name = "Man_MDA_vacc.txt",
-        # the following number dictates the number of events (e.g. worm deaths)
-        # we allow to happen before updating other parts of the model
-        # the higher this number the faster the simulation
-        # (though there is a check so that there can't be too many events at once)
-        # the higher the number the greater the potential for
-        # errors in the model accruing.
-        # 5 is a reasonable level of compromise for speed and errors, but using
-        # a lower value such as 3 is also quite good
-        min_multiplier = 5L
-    )
-
-        # Example prevalence map, with two locations, fitting to two time times
+    # Example prevalence map, with two locations, fitting to two time times
     # Both locations start at 0.031, and the second time point is 0.021
     prevalence_map <- vector("list", 2)
     prevalence_map[[1]]$data <- matrix(c(0.031, 0.031))
@@ -193,36 +96,12 @@ test_that("Running the AMIS integration on multiple time points should complete 
 
     expect_error(AMISforInfectiousDiseases::amis(
         prevalence_map,
-        build_transmission_model(prevalence_map, fixed_parameters, year_indices),
+        build_transmission_model(prevalence_map, example_parameters, year_indices),
         prior
     ), "(No weight on any particles for locations in the active set.)|(the leading minor of order 2 is not positive definite)")
 })
 
 test_that("Running the AMIS integration should complete with the error about weight on particles", {
-    sch_simulation <- get_amis_integration_package()
-
-    fixed_parameters <- sch_simulation$FixedParameters(
-        # the higher the value of N, the more consistent the results will be
-        # though the longer the simulation will take
-        number_hosts = 10L,
-        # no intervention
-        coverage_file_name = "mansoni_coverage_scenario_0.xlsx",
-        demography_name = "UgandaRural",
-        # cset the survey type to Kato Katz with duplicate slide
-        survey_type = "KK2",
-        parameter_file_name = "mansoni_params.txt",
-        coverage_text_file_storage_name = "Man_MDA_vacc.txt",
-        # the following number dictates the number of events (e.g. worm deaths)
-        # we allow to happen before updating other parts of the model
-        # the higher this number the faster the simulation
-        # (though there is a check so that there can't be too many events at once)
-        # the higher the number the greater the potential for
-        # errors in the model accruing.
-        # 5 is a reasonable level of compromise for speed and errors, but using
-        # a lower value such as 3 is also quite good
-        min_multiplier = 5L
-    )
-
     # Example prevalence map, with two locations, both with prevalence of 0.5
     prevalence_map <- matrix(c(0.5, 0.5), ncol = 1)
 
@@ -244,7 +123,7 @@ test_that("Running the AMIS integration should complete with the error about wei
 
     expect_error(AMISforInfectiousDiseases::amis(
         prevalence_map,
-        build_transmission_model(prevalence_map, fixed_parameters, year_indices = c(23)),
+        build_transmission_model(prevalence_map, example_parameters, year_indices = c(23)),
         prior
     ), "(No weight on any particles for locations in the active set.)|(the leading minor of order 2 is not positive definite)")
 })
