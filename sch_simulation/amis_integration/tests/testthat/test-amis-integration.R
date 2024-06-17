3c224bf44701db91334f93f7637f113c9253769d
Thomas <thomas.kiley@ndm.ox.ac.uk>
2024-06-17 12:17:56 +0100

    Add integration test for multiple time points

--- sch_simulation/amis_integration/tests/testthat/test-amis-integration.R
+++ sch_simulation/amis_integration/tests/testthat/test-amis-integration.R
@@ -73,6 +73,62 @@ test_that("Running the simulation on multiple time points gives multiple points 
     expect_equal(result, matrix(c(0.1, 0.1, 0.1, 0.1), ncol = 2), tolerance = 0.5)
 })
 
+test_that("Running the AMIS integration on multiple time points should complete with the error about weight on particles", {
+    sch_simulation <- get_amis_integration_package()
+
+    fixed_parameters <- sch_simulation$FixedParameters(
+        # the higher the value of N, the more consistent the results will be
+        # though the longer the simulation will take
+        number_hosts = 10L,
+        # no intervention
+        coverage_file_name = "mansoni_coverage_scenario_0.xlsx",
+        demography_name = "UgandaRural",
+        # cset the survey type to Kato Katz with duplicate slide
+        survey_type = "KK2",
+        parameter_file_name = "mansoni_params.txt",
+        coverage_text_file_storage_name = "Man_MDA_vacc.txt",
+        # the following number dictates the number of events (e.g. worm deaths)
+        # we allow to happen before updating other parts of the model
+        # the higher this number the faster the simulation
+        # (though there is a check so that there can't be too many events at once)
+        # the higher the number the greater the potential for
+        # errors in the model accruing.
+        # 5 is a reasonable level of compromise for speed and errors, but using
+        # a lower value such as 3 is also quite good
+        min_multiplier = 5L
+    )
+
+        # Example prevalence map, with two locations, fitting to two time times
+    # Both locations start at 0.031, and the second time point is 0.021
+    prevalence_map <- vector("list", 2)
+    prevalence_map[[1]]$data <- matrix(c(0.031, 0.031))
+    prevalence_map[[2]]$data <- matrix(c(0.021, 0.021))
+
+    year_indices <- c(0L, 23L)
+
+    #' the "dprior" function
+    #' Note the second parameter _must_ be called log
+    #' Unclear what this function does
+    density_function <- function(parameters, log) {
+        return(0.5)
+    }
+
+    #' The "rprior" function that returns a matrix whose columns are the parameters
+    #' and each row is a sample
+    rnd_function <- function(num_samples) {
+        return(matrix(c(3, 0.04), ncol = 2, nrow = num_samples, byrow = TRUE))
+    }
+
+    prior <- list("dprior" = density_function, "rprior" = rnd_function)
+
+
+    expect_error(AMISforInfectiousDiseases::amis(
+        prevalence_map,
+        build_transmission_model(prevalence_map, fixed_parameters, year_indices),
+        prior
+    ), "(No weight on any particles for locations in the active set.)|(the leading minor of order 2 is not positive definite)")
+})
+
 test_that("Running the AMIS integration should complete with the error about weight on particles", {
     sch_simulation <- get_amis_integration_package()
 
