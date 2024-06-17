36c61b506b8686eccd74fbbbb1a5fb1e02a1782a
Thomas <thomas.kiley@ndm.ox.ac.uk>
2024-06-17 12:14:02 +0100

    Add test for checking the python integration for multiple points
    Verifies we get a 2x2 matrix back

--- sch_simulation/amis_integration/tests/testthat/test-amis-integration.R
+++ sch_simulation/amis_integration/tests/testthat/test-amis-integration.R
@@ -35,6 +35,44 @@ test_that("Running the model should give us some results", {
     expect_equal(result, matrix(c(0.1, 0.1), ncol = 1), tolerance = 0.5)
 })
 
+test_that("Running the simulation on multiple time points gives multiple points back", {
+    sch_simulation <- get_amis_integration_package()
+
+    fixed_parameters <- sch_simulation$FixedParameters(
+        # the higher the value of N, the more consistent the results will be
+        # though the longer the simulation will take
+        number_hosts = 10L,
+        # no intervention
+        coverage_file_name = "mansoni_coverage_scenario_0.xlsx",
+        demography_name = "UgandaRural",
+        # cset the survey type to Kato Katz with duplicate slide
+        survey_type = "KK2",
+        parameter_file_name = "mansoni_params.txt",
+        coverage_text_file_storage_name = "Man_MDA_vacc.txt",
+        # the following number dictates the number of events (e.g. worm deaths)
+        # we allow to happen before updating other parts of the model
+        # the higher this number the faster the simulation
+        # (though there is a check so that there can't be too many events at once)
+        # the higher the number the greater the potential for
+        # errors in the model accruing.
+        # 5 is a reasonable level of compromise for speed and errors, but using
+        # a lower value such as 3 is also quite good
+        min_multiplier = 5L
+    )
+
+    # Example prevalence map, with two locations, fitting to two time times
+    # Both locations start at 0.031, and the second time point is 0.021
+    prevalence_map <- vector("list", 2)
+    prevalence_map[[1]]$data <- matrix(c(0.031, 0.031))
+    prevalence_map[[2]]$data <- matrix(c(0.021, 0.021))
+
+    year_indices <- c(0L, 23L)
+
+    tranmission_model <- build_transmission_model(prevalence_map, fixed_parameters, year_indices)
+    result <- tranmission_model(c(1, 2), matrix(c(3, 3, 0.04, 0.04), ncol = 2), 1)
+    expect_equal(result, matrix(c(0.1, 0.1, 0.1, 0.1), ncol = 2), tolerance = 0.5)
+})
+
 test_that("Running the AMIS integration should complete with the error about weight on particles", {
     sch_simulation <- get_amis_integration_package()
 
