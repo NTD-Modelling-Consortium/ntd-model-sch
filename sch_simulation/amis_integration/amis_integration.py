8aa450a157535dd495b6d215189f5a035b705050
Thomas <thomas.kiley@ndm.ox.ac.uk>
2024-06-18 10:11:24 +0100

    Don't cache results
    Now we are providing a seed there will be no benefit for caching but it
    will still use lots of memory

--- sch_simulation/amis_integration/amis_integration.py
+++ sch_simulation/amis_integration/amis_integration.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from functools import cache
+import random
 from typing import Literal
 import pandas as pd
 import sch_simulation
@@ -44,8 +44,7 @@ class FixedParameters:
     A higher number will result in faster but less accurate simulation."""
 
 
-@cache
-def returnYearlyPrevalenceEstimate(R0, k, fixed_parameters: FixedParameters):
+def returnYearlyPrevalenceEstimate(R0, k, seed, fixed_parameters: FixedParameters):
     cov = parse_coverage_input(
         fixed_parameters.coverage_file_name,
         fixed_parameters.coverage_text_file_storage_name,
