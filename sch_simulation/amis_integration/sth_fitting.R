6cab790fcdb48ca2058760ce07b86fc3d5bbc7c6
Raiha Browning <raiha_p123@hotmail.com>
2024-07-11 13:13:13 +0100

    Update nHosts and parameter file names and save yearly trajectory from simulations. Also add batch ID, prevalence maps, priors, AMIS parameters and save outputs.

--- sch_simulation/amis_integration/sth_fitting.R
+++ sch_simulation/amis_integration/sth_fitting.R
@@ -1,4 +1,14 @@
-source("amis_integration.R")
+
+# on cluster:
+# id = as.numeric(Sys.getenv("SLURM_ARRAY_TASK_ID"))
+# for testing:
+id = 1
+
+library(dplyr)
+library(AMISforInfectiousDiseases)
+
+# assuming working directory is ntd-model-sch
+source("./sch_simulation/amis_integration/amis_integration.R")
 
 args <- commandArgs(trailingOnly=TRUE)
 num_cores_to_use <- parallel::detectCores()
@@ -13,13 +23,13 @@ sch_simulation <- get_amis_integration_package()
 fixed_parameters <- sch_simulation$FixedParameters(
     # the higher the value of N, the more consistent the results will be
     # though the longer the simulation will take
-    number_hosts = 10L,
+    number_hosts = 500L,
     # no intervention
-    coverage_file_name = "mansoni_coverage_scenario_0.xlsx",
+    coverage_file_name = paste0("endgame_inputs/InputMDA_MTP_",id,".xlsx"),
     demography_name = "UgandaRural",
     # cset the survey type to Kato Katz with duplicate slide
     survey_type = "KK2",
-    parameter_file_name = "mansoni_params.txt",
+    parameter_file_name = "STH_params/ascaris_params.txt",
     coverage_text_file_storage_name = "Man_MDA_vacc.txt",
     # the following number dictates the number of events (e.g. worm deaths)
     # we allow to happen before updating other parts of the model
@@ -32,37 +42,64 @@ fixed_parameters <- sch_simulation$FixedParameters(
     min_multiplier = 5L
 )
 
-# Example prevalence map, with two locations, fitting to two time times
-# Both locations start at 0.031, and the second time point is 0.021
-prevalence_map <- vector("list", 2)
-prevalence_map[[1]]$data <- matrix(c(0.031, 0.031))
-prevalence_map[[2]]$data <- matrix(c(0.021, 0.021))
 
-#' the "dprior" function
-#' Note the second parameter _must_ be called log
-#' Unclear what this function does
-density_function <- function(parameters, log) {
-    return(0.5)
-}
+# Load prevalence map and filter rows for TaskID == id
+year_indices <- c(15L,28L,33L) # 2000, 2013, 2018 (end of year in model)
+load("../Maps/ascaris_maps.rds")
+prevalence_map = ascaris_map_allyears
+prevalence_map = lapply(1:length(prevalence_map), function(t){
+  output=list(data = as.matrix(prevalence_map[[t]]$data %>% 
+                                 filter(TaskID==id) %>% 
+                                 select(mu, sigma)), 
+              likelihood = prevalence_map[[t]]$likelihood)
+  rownames(output$data) = rownames(prevalence_map[[t]]$data)[prevalence_map[[t]]$data$TaskID==id]
+  return(output)
+  
+})
 
-#' The "rprior" function that returns a matrix whose columns are the parameters
-#' and each row is a sample
-rnd_function <- function(num_samples) {
-    return(matrix(c(3, 0.04), ncol = 2, nrow = num_samples, byrow = TRUE))
-}
+# load prior
+source("../Ascaris_prior_mixed.R")
+prior = MixedPrior
 
-prior <- list("dprior" = density_function, "rprior" = rnd_function)
+# Algorithm parameters
+amis_params<-default_amis_params()
+amis_params$max_iters=12
+amis_params$n_samples=1000
+amis_params$target_ess =500
+amis_params$sigma=0.0025
+amis_params$boundaries=c(-Inf,Inf)
+amis_params$boundaries_param = matrix(c(R_lb,k_lb,R_ub,k_ub),ncol=2)
 
-year_indices <- c(0L, 23L)
+# shell to save trajectories
+trajectories = c() # save simulated trajectories as code is running
+save(trajectories,file=paste0("trajectories_",id,".Rdata"))
 
-
+# Run AMIS
+st<-Sys.time()
 amis_output <- AMISforInfectiousDiseases::amis(
     prevalence_map,
     build_transmission_model(prevalence_map, fixed_parameters, year_indices, num_cores_to_use),
-    prior
+    prior,
+    amis_params,
+    seed = id
 )
+en<-Sys.time()
+dur_amis<-as.numeric(difftime(en,st,units="mins"))
+if (!dir.exists("../AMIS_output")) {dir.create("../AMIS_output")}
+save(amis_output,file=paste0("../AMIS_output/amis_output",id,".Rdata"))
+
 
 # Currently errors - I think because I
 # don't know where the weights need to be set
 # "No weight on any particles for locations in the active set."
 print(amis_output)
+
+# save summary
+ess<-amis_output$ess
+n_success<-length(which(ess>=amis_params[["target_ess"]]))
+failures<-which(ess<amis_params[["target_ess"]])
+n_failure<-length(failures)
+if (n_failure>0) {cat(paste(failures,id,ess[failures]),file = "../ESS_NOT_REACHED.txt",sep = "\n", append = TRUE)}
+if (!file.exists("../summary.csv")) {cat("ID,n_failure,n_success,n_sim,min_ess,duration_amis,durarion_subsampling\n",file="../summary.csv")}
+cat(id,n_failure,n_success,length(amis_output$seeds),min(ess),dur_amis,NA,"\n",sep=",",file="../summary.csv",append=TRUE)
+
