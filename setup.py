620f54ee187082db9466b56ebbe5d8be44116eea
Evandro Konzen <stsnmt@login01.avon.hpc>
2024-08-27 07:57:46 +0100

    try specific version of python_requires in setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     description='SCH simulation model',
     long_description='Individual-based model in Medley 1989 thesis and Anderson&Medley 1985.',
     packages=setuptools.find_packages(),
-    python_requires='>=3.6,<3.11',
+    python_requires='==3.10.4',
     install_requires=['numpy<2.0', 'scipy', 'pandas', 'joblib', 'openpyxl', 'pytest'],
     include_package_data=True
 )
